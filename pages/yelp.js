14c50fc2ffb1779d12b702fbe56c3831582e8e3c
Ygoraphael <ygoraphael@gmail.com>
2022-10-26 15:45:45 +0100

    TESTE CORS

--- pages/yelp.js
+++ pages/yelp.js
@@ -4,21 +4,18 @@ export default function Yelp() {
     const [reviews, setReviews] = useState(null);
     
     const APIYelp = async () => {
-
-        fetch(`https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews`, {
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews`, {
             headers: {
                 Authorization: `Bearer tEOdJQPpkqhMr5iQswLX-vHYq5C4vy-C65X0EjO07sfTqX1QMQD-oLFApPj0h4SxJw5Jy6gh5YKsGE5OrjXpagmiwVXmluFUBtYm4f9o_kPGQ1FBTtgLVIuXYAtOY3Yx`,
                 Origin: 'localhost',
                 withCredentials: true,
             }
         }).then(res => res.json())
-        .then(json => setReviews(json.reviews))
-        .catch(err => console.error('error:' + err));
-
-        console.log("1")
-        console.log(reviews)
-        console.log("2")
-            
+        .then(json => {
+            console.log(json.reviews);
+            setReviews(json.reviews);
+        })
+        .catch(err => console.error('error:' + err));    
     }
 
     useEffect(() => { APIYelp() }, []);
