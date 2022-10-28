44e2b75094bcdae6520b613c6d4de600383e6354
Ygor Raphael <ygoraphael@gmail.com>
2022-10-28 02:50:08 +0100

    Update yelp.js

--- pages/yelp.js
+++ pages/yelp.js
@@ -4,7 +4,7 @@ export default function Yelp() {
     const [reviews, setReviews] = useState(null);
     
     const APIYelp = async () => {
-        fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews`, {
+        fetch(`https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews`, {
             headers: {
                 Authorization: `Bearer tEOdJQPpkqhMr5iQswLX-vHYq5C4vy-C65X0EjO07sfTqX1QMQD-oLFApPj0h4SxJw5Jy6gh5YKsGE5OrjXpagmiwVXmluFUBtYm4f9o_kPGQ1FBTtgLVIuXYAtOY3Yx`,
                 Origin: 'localhost',
