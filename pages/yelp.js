e515661f10a1de30f51ec2f5a2b8ed74895b4162
Ygor Raphael <ygoraphael@gmail.com>
2022-10-28 06:06:06 +0100

    Update yelp.js

--- pages/yelp.js
+++ pages/yelp.js
@@ -3,12 +3,11 @@ import { useEffect, useState } from 'react';
 export default function Yelp() {
     const [reviews, setReviews] = useState([]);
     const apiKey = process.env.NEXT_PUBLIC_API_SECRET;
-    console.log(apiKey)
     
     const APIYelp = async () => {
         fetch(`/Reviews`, {
             headers: {
-                Authorization: `Bearer tEOdJQPpkqhMr5iQswLX-vHYq5C4vy-C65X0EjO07sfTqX1QMQD-oLFApPj0h4SxJw5Jy6gh5YKsGE5OrjXpagmiwVXmluFUBtYm4f9o_kPGQ1FBTtgLVIuXYAtOY3Yx`,
+                Authorization: `Bearer ${apiKey}`,
             }
         }).then(res => res.json())
         .then(json => { setReviews(json.reviews) })
